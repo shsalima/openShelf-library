3e530215e53f84f68291ec15a397e099350754b5
salima <sahisalima77@gmail.com>
2026-07-15 13:09:16 +0200

    travail sur functionnalité de ajouter la logique

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,4 +1,5 @@
 import { connectDB } from "@/lib/db";
+import { BookSchema } from "@/lib/validators";
 import Book from "@/models/Book";
 import {  NextResponse } from "next/server";
 
@@ -19,4 +20,32 @@ export async function GET() {
       { status: 500 },
     );
   }
+}
+
+
+export async function POST(request:Request){
+    try{
+        await connectDB()
+        const body =await request.json()
+
+        const result =BookSchema.safeParse(body)
+
+        if(!result.success){
+            return NextResponse.json(
+                {
+                    message:"Erreur de validation",
+                    errors:result.error.flatten().fieldErrors,
+                },
+                {status:400}
+            )
+        }
+        const book= await Book.create(result.data)
+        return NextResponse.json(book,{status:201})
+    }catch(error){
+        console.error(error)
+        return NextResponse.json(
+            {message:"Erreur serveur"},
+            {status:500}
+        )
+    }
 }
